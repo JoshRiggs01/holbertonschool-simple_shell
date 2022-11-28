cbbc35decb33a8c8430fa76e8fe22ca94e16326d
JoshRiggs01 <5574@holbertonstudents.com>
2022-11-28 11:42:10 -0800

    0 pid and 0av

---
+++ av.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints all args without ac
+ * @ac: numberof arg
+ * @av: array of strings
+ *
+ * Return: 0 if success
+ */
+
+int main(int ac, char **av)
+{
+	int it;
+	for (it = 0; it < ac; it++)
+	{
+		(void) ac;
+		printf("%s\n", av[i]);
+	}
+	return (0);
+}
