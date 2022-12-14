647e5c180fde75ea714289dd1423c817979d77ef
JoshRiggs01 <5574@holbertonstudents.com>
2022-12-13 22:03:17 -0800

    simple shell

--- simple_shell.c
+++ simple_shell.c
@@ -8,54 +8,37 @@
 #include <dirent.h>
 #include <signal.h>
 #include <stdlib.h>
-
+/**
+ * main - entry point
+ */
 /** Define a constant for the maximum length of a command*/
 #define MAX_COMMAND_LENGTH 100
-
-int main() 
+int main(void)
 {
-    /** Declare the command variable at the beginning of the function*/
-    char command[MAX_COMMAND_LENGTH];
-	/** declating the length and result variable outside of the while lopp*/
-    int length = 0;
-    int result = 0;
+	char command[MAX_COMMAND_LENGTH]; /** declaring command var name*/
+	int length = 0;
+	int result = 0; /** declating the length and result variable outside lopp*/
 
-    /**
-     * Loop until the user enters the "exit" command.
-     */
-    while (1) 
-    {
-        /** Display the prompt and read the user's input*/
-        printf("($) ");
-        if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL) 
+	while (1) /** Loop until the user enters the "exit" command.*/
 	{
-            /** If end of file is reached (Ctrl+D), exit the program*/
-            break;
-        }
-
-        /** Remove the trailing newline character from the command*/
-       	length = strlen(command);
-        if (length > 0 && command[length - 1] == '\n') 
-	{
-            command[length - 1] = 0;
-        }
-
-        /** If the user entered the "exit" command, exit the program*/
-        if (strcmp(command, "exit") == 0) 
-	{
-            break;
-        }
-
-        /** Try to execute the command using the system() function*/
-       	result = system(command);
-
-        /** If the command was not found, print an error message*/
-        if (result == 127) 
-	{
-            fprintf(stderr, "simple_shell: command not found: %s\n", command);
-        }
-    }
-
-    return (0);
+		printf("($) ");
+		if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL)
+		{
+			break; /** If end of file is reached (Ctrl+D), exit the program*/
+		}
+		length = strlen(command); /** Remove the trailing \n frm cmd*/
+		if (length > 0 && command[length - 1] == '\n')
+		{
+			command[length - 1] = 0;
+		}
+			if (strcmp(command, "exit") == 0)
+			{
+				break;
+			}
+			if (result == 127) /**command not found print error*/
+			{
+				fprintf(stderr, "simple_shell: command not found: %s\n", command);
+			}
+	}
+	return (0);
 }
-
