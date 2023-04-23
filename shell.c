3f516956efe3bf49cfe2f34df80d0687ee423aca
JoshRiggs01 <5574@holbertonstudents.com>
2023-04-23 09:17:55 -0700

    how about now?

--- shell.c
+++ shell.c
@@ -1,4 +1,13 @@
-#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <signal.h>
+#include <stdlib.h>
 /**
  * main - entry point
  */
@@ -9,19 +18,15 @@ int main(void)
 	char command[MAX_COMMAND_LENGTH]; /** declaring command var name*/
 	int length = 0;
 	int result = 0; /** declating the length and result variable outside lopp*/
-	char buf[1024];
 
 	while (1) /** Loop until the user enters the "exit" command.*/
 	{
 		if (isatty(fileno(stdin)))
-		{
-			write(STDOUT_FILENO, "($) ", 4);
-		}
+		printf("($) ");
 
 		if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL)
 		{
 			break; /** If end of file is reached (Ctrl+D), exit the program*/
-			exit(EXIT_SUCCESS);
 		}
 		length = strlen(command); /** Remove the trailing \n frm cmd*/
 		if (length > 0 && command[length - 1] == '\n')
@@ -36,26 +41,6 @@ int main(void)
 			if (result == 127) /**command not found print error*/
 			{
 				fprintf(stderr, "simple_shell: command not found: %s\n", command);
-			}
-			else if (result == 0)
-			{
-				FILE *fp = popen(command, "r");
-				if (fp == NULL)
-				{
-					perror("popen");
-					exit(EXIT_FAILURE);
-				}
-				while (fgets(buf, sizeof(buf), fp) != NULL)
-			{
-				char *token = strtok(buf, "\n");
-				while (token != NULL)
-				{
-					write(STDOUT_FILENO, token, strlen(token));
-					write(STDOUT_FILENO, "\n", 1);
-					token = strtok(NULL, "\n");
-				}
-			}
-			pclose(fp);
 			}
 	}
 	return (0);
